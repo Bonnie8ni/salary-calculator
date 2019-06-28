48f8ef17bc9384c565b2fa583f128cc213b7b7dc
BonnieYu <Bonnie_Yu@wistron.com>
2019-06-29 01:28:40 +0800

    Updates

---
+++ precache-manifest.2ca34c6cb4c083861e0205468532ae04.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f959d9c6ea69f193a9369c5cdd69b487",
+    "url": "/salary-calculator/index.html"
+  },
+  {
+    "revision": "43beb4689dff68018535",
+    "url": "/salary-calculator/static/js/2.e7dbb282.chunk.js"
+  },
+  {
+    "revision": "ea13f419132b45501fa1",
+    "url": "/salary-calculator/static/js/main.444e96f0.chunk.js"
+  },
+  {
+    "revision": "cbbf00574a7c99c7808d",
+    "url": "/salary-calculator/static/js/runtime~main.87cdd804.js"
+  }
+]);
